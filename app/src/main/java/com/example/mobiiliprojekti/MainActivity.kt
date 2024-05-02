16c9a85916be99f0127e80c6ef6442274384788f
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2024-05-02 14:58:02 +0300

    Preparing of bottom navbar and appbar for application

--- app/src/main/java/com/example/mobiiliprojekti/MainActivity.kt
+++ app/src/main/java/com/example/mobiiliprojekti/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.mobiiliprojekti
 
 import android.os.Bundle
-import android.view.Window
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mobiiliprojekti.databinding.ActivityMainBinding
 
