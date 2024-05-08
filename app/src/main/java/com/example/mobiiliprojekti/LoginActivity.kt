d0e1f8a6a0fb5ef86332cdc3ff2d466cfd899ab3
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2024-05-08 15:23:00 +0300

    dialog fragments for login, register and forgot password,

--- app/src/main/java/com/example/mobiiliprojekti/LoginActivity.kt
+++ app/src/main/java/com/example/mobiiliprojekti/LoginActivity.kt
@@ -1,11 +1,42 @@
 package com.example.mobiiliprojekti
 
+import LoginFragment
+import RegisterFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // set register button to variable
+        val btnRegister: Button = findViewById(R.id.btn_register)
+
+        // add onClickListener to that button
+        btnRegister.setOnClickListener {
+            // set register fragment to variable
+            val fragmentRegister = RegisterFragment()
+
+            // show fragment as dialog
+            supportFragmentManager.beginTransaction()
+                .add(fragmentRegister, "register_dialog")
+                .commit()
+        }
+
+        // set login button to variable
+        val btnLogin: Button = findViewById(R.id.btn_login)
+
+        // add onClickListener to that button
+        btnLogin.setOnClickListener {
+            // set login fragment to variable
+            val fragmentLogin = LoginFragment()
+
+            // show fragment as dialog
+            supportFragmentManager.beginTransaction()
+                .add(fragmentLogin, "register_dialog")
+                .commit()
+        }
     }
 }
