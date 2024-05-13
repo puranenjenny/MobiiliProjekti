6ab1bbc1c2d2d727054476ca79d73a5541e7ec6e
Jenny Puranen <124900133+puranenjenny@users.noreply.github.com>
2024-05-13 14:31:25 +0300

    minSdk päivitetty

--- app/src/main/java/com/example/mobiiliprojekti/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mobiiliprojekti/ui/dashboard/DashboardFragment.kt
@@ -1,24 +1,27 @@
 package com.example.mobiiliprojekti.ui.dashboard
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.mobiiliprojekti.DatabaseManager
 import com.example.mobiiliprojekti.databinding.FragmentDashboardBinding
+import java.time.LocalDate
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     // db-manager object
     private lateinit var databaseManager: DatabaseManager
+
+
+    private var currentMonthIndex: Int = LocalDate.now().monthValue
 
     override fun onCreateView(
         inflater: LayoutInflater,
