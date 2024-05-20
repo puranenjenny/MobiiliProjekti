13d1e99b3207183cc0dab64c42167b1ba8e384ba
Jenny Puranen <124900133+puranenjenny@users.noreply.github.com>
2024-05-20 11:06:23 +0300

    Update HomeFragment.kt

--- app/src/main/java/com/example/mobiiliprojekti/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mobiiliprojekti/ui/home/HomeFragment.kt
@@ -103,8 +103,9 @@ private fun displayLastPurchases() {
         purchasesLayout.addView(noPurchasesView)
     } else {
         purchases.forEach { purchase ->
-            val purchaseView = TextView(context).apply { //POISTA KATEGORIA!! 
-                text = "${purchase.date}: ${purchase.category} - ${purchase.name} - ${purchase.price} €"
+            val categoryName = databaseManager.getCategoryNameById(purchase.category)
+            val purchaseView = TextView(context).apply {
+                text = "${purchase.date} - $categoryName - ${purchase.name} - ${purchase.price} €"
                 textSize = 18f
                 setPadding(20, 20, 20, 20)
                 isClickable = true
@@ -117,6 +118,8 @@ private fun displayLastPurchases() {
         }
     }
 }
+
+
 
 
     private fun showEditPurchaseDialog(purchase: Purchase) {
